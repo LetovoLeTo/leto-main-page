787681925f4171399c9c11f3dc17c854221343a1
Milk-Cool <milan-pl@mail.ru>
2023-08-14 21:48:42 +0300

    started working on main page and updated the style submodule

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const app = express();
 const port = 3000;
 
 app.use("/res", express.static("leto-resources"));
+app.use("/", express.static("html"));
+app.get("/", (req, res) => {
+    res.sendFile("html/index.html");
+});
 
-app.listen(port, () => console.log("Server's alive!"))
+app.listen(port, () => console.log("Server's alive!"));
