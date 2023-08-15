9384045dec5116f06d56f6ba1d629cfb74462bf3
Milk-Cool <milan-pl@mail.ru>
2023-08-15 19:30:30 +0300

    removed the resources submodule

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use("/res", express.static("leto-resources"));
 app.use("/", express.static("html"));
 app.get("/", (req, res) => {
     res.sendFile("html/index.html");
