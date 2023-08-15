3e642e7e8bf9977c2b78fff8d3e1f17e52fcaabe
Milk-Cool <milan-pl@mail.ru>
2023-08-15 19:47:32 +0300

    now using __dirname to avoid issues

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const { join } = require("path");
 
 const app = express();
 
 app.use("/", express.static("html"));
 app.get("/", (req, res) => {
-    res.sendFile("html/index.html");
+    res.sendFile(join(__dirname, "html/index.html"));
 });
 
 module.exports = app;
